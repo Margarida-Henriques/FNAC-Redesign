9a5f9c62e53ac4d145f133a08d72e4639c8c95d6
Margarida Henriques <margaridahenriques934@gmail.com>
2025-01-04 13:06:13 +0000

    Created Routes / Added CORS

--- backend/index.js
+++ backend/index.js
@@ -1,125 +1,26 @@
 import express, { request, response } from "express";
 import mongoose from "mongoose";
 import { PORT, mongoDBURL } from "./config.js";
-import { Product } from "./models/productModel.js";
-
+import productsRoute from './routes/productsRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json())
 
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
+
 app.get('/', (request, response) => {
-    // console.log(request);
     return response.status(234).send('Get response')
 });
 
-
-//POST PRODUCT
-app.post('/products', async (request, response) => {
-    try {
-        if (
-            !request.body.name ||
-            !request.body.price ||
-            !request.body.category
-        ) {
-            return response.status(400).send({
-                message: 'Send all required fields: name, price, category'
-            })
-        };
-
-        const newProduct = {
-            name: request.body.name,
-            price: request.body.price,
-            category: request.body.category,
-        };
-
-        const product = await Product.create(newProduct);
-        return response.status(201).send(product);
-
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
-
-//GET PRODUCTS
-app.get('/products', async (request, response) => {
-    try {
-        const products = await Product.find({});
-
-        return response.status(200).json(products)
-
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
-
-//GET PRODUCT
-app.get('/products/:id', async (request, response) => {
-    try {
-
-        const { id } = request.params;
-        const product = await Product.findById(id);
-
-        return response.status(200).json(product)
-
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
-
-//PUT PRODUCT
-app.put('/products/:id', async (request, response) => {
-    try {
-        if (
-            !request.body.name ||
-            !request.body.price ||
-            !request.body.category
-        ) {
-            return response.status(400).send({
-                message: 'Send all required fields: name, price, category'
-            })
-        };
-
-        const { id } = request.params;
-
-        const result = await Product.findByIdAndUpdate(id, request.body);
-
-        if (!result) {
-            return response.status(404).json({ message: 'Product not found' });
-        } else {
-            return response.status(200).send({ message: 'Product updated successfully' });
-        }
-
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
-
-//DELETE PRODUCT
-app.delete('/products/:id', async (request, response) => {
-    try {
-        const { id } = request.params;
-
-        const result = await Product.findByIdAndDelete(id);
-
-        if (!result) {
-            return response.status(404).json({ message: 'Product not found' });
-        } else {
-            return response.status(200).send({ message: 'Product deleted successfully' });
-        }
-
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message })
-    }
-});
-
-
-
+app.use('/products', productsRoute);
 
 
 mongoose
---
+++ backend/routes/productsRoute.js
@@ -0,0 +1,110 @@
+import express from 'express';
+import { Product } from "../models/productModel.js";
+
+const router = express.Router();
+
+//POST PRODUCT
+router.post('/', async (request, response) => {
+    try {
+        if (
+            !request.body.name ||
+            !request.body.price ||
+            !request.body.category
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: name, price, category'
+            })
+        };
+
+        const newProduct = {
+            name: request.body.name,
+            price: request.body.price,
+            category: request.body.category,
+        };
+
+        const product = await Product.create(newProduct);
+        return response.status(201).send(product);
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+//GET PRODUCTS
+router.get('/', async (request, response) => {
+    try {
+        const products = await Product.find({});
+
+        return response.status(200).json(products)
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+//GET PRODUCT
+router.get('/:id', async (request, response) => {
+    try {
+
+        const { id } = request.params;
+        const product = await Product.findById(id);
+
+        return response.status(200).json(product)
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+//PUT PRODUCT
+router.put('/:id', async (request, response) => {
+    try {
+        if (
+            !request.body.name ||
+            !request.body.price ||
+            !request.body.category
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: name, price, category'
+            })
+        };
+
+        const { id } = request.params;
+
+        const result = await Product.findByIdAndUpdate(id, request.body);
+
+        if (!result) {
+            return response.status(404).json({ message: 'Product not found' });
+        } else {
+            return response.status(200).send({ message: 'Product updated successfully' });
+        }
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+//DELETE PRODUCT
+router.delete('/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+
+        const result = await Product.findByIdAndDelete(id);
+
+        if (!result) {
+            return response.status(404).json({ message: 'Product not found' });
+        } else {
+            return response.status(200).send({ message: 'Product deleted successfully' });
+        }
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message })
+    }
+});
+
+export default router;
